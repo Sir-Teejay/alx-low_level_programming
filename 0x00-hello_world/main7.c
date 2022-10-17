818753c06a686ab82b74be7d4e09ab3c79a9cf0a
Umar Tijjani <sirtiijay@gmail.com>
2022-10-17 13:58:32 +0100

    project 6

---
+++ 0x00-hello_world/main7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+	*main- serves as the entry point for the program
+	*
+	*Return: By default returns zero if no erros otherwise returns non-zero value
+	*/
+int main(void)
+{
+	return (0);
+}
